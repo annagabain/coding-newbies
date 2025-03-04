1d3e412a9de2ad34a2da84c84393004cd8af44d5
AG <anna.gabain@gmail.com>
2025-03-04 15:34:13 +0100

    add email contact

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,10 +9,10 @@ function App() {
         <img src={logo} className="App-new-logo" alt="logo" />
 
         <h1>REGISTER</h1>
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>
+          Contact me for registration and questions about the course at anna.gabain@outlook.com
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
